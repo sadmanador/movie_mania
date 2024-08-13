0bf65dea50d96e7104c4150079d3d9abc36ff54d
sadmanador <sadman.ador@gmail.com>
2024-08-13 22:15:34 +0600

    navbar fixed

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,98 +5,109 @@ import SearchBox from "../SearchBox/SearchBox";
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-300 sticky top-0 z-20">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+    <>
+      <div className="sm:hidden navbar bg-base-300">
+        <a className="btn btn-ghost text-xl">Movie Mania</a>
+      </div>
+      <div className="navbar bg-base-300 sticky top-0 z-20">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
+              <li className="mx-auto">
+                <div>
+                  <ul
+                    tabIndex={0}
+                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+                  >
+                    <li>
+                      <Link
+                        className="hover:text-yellow-500 text-sm"
+                        href={"/"}
+                      >
+                        Home
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        className="hover:text-yellow-500 text-sm"
+                        href={"/explore"}
+                      >
+                        Explore
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        className="hover:text-yellow-500 text-sm"
+                        href={"/tv_shows"}
+                      >
+                        TV Shows
+                      </Link>
+                    </li>
+                    <li>
+                      <ThemeToggleButton />
+                    </li>
+                  </ul>
+                </div>
+              </li>
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          >
-            <li className="mx-auto">
-              <div>
-                <ul
-                  tabIndex={0}
-                  className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-                >
-                  <li>
-                    <Link className="hover:text-yellow-500 text-sm" href={"/"}>
-                      Home
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      className="hover:text-yellow-500 text-sm"
-                      href={"/explore"}
-                    >
-                      Explore
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      className="hover:text-yellow-500 text-sm"
-                      href={"/tv_shows"}
-                    >
-                      TV Shows
-                    </Link>
-                  </li>
-                  <li>
-                    <ThemeToggleButton />
-                  </li>
-                </ul>
-              </div>
+          <Link href={"/"} className="lg:text-xl hidden sm:block">
+            Movie Mania
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className=" px-1 flex items-center lg:gap-4 gap-1">
+            <li>
+              <Link className="hover:text-yellow-500 text-sm" href={"/"}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className="hover:text-yellow-500 text-sm" href={"/explore"}>
+                Explore
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="hover:text-yellow-500 text-sm"
+                href={"/tv_shows"}
+              >
+                TV Shows
+              </Link>
+            </li>
+            <li>
+              <ThemeToggleButton />
             </li>
           </ul>
         </div>
-        <Link href={"/"} className="lg:btn btn-ghost lg:text-xl">
-          Movie Mania
-        </Link>
+        <div className="navbar-end">
+          <ul className="px-1">
+            <li className="relative">
+              <SearchBox />
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className=" px-1 flex items-center lg:gap-4 gap-1">
-          <li>
-            <Link className="hover:text-yellow-500 text-sm" href={"/"}>
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link className="hover:text-yellow-500 text-sm" href={"/explore"}>
-              Explore
-            </Link>
-          </li>
-          <li>
-            <Link className="hover:text-yellow-500 text-sm" href={"/tv_shows"}>
-              TV Shows
-            </Link>
-          </li>
-          <li>
-            <ThemeToggleButton />
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <ul className="px-1">
-          <li className="relative">
-            <SearchBox />
-          </li>
-        </ul>
-      </div>
-    </div>
+    </>
   );
 };
 
