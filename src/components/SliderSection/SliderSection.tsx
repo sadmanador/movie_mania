ca605147b490bcf438a0865f02fb627c7d6016da
sadmanador <sadman.ador@gmail.com>
2024-08-17 00:02:50 +0600

    cosmetic adjustment

--- src/components/SliderSection/SliderSection.tsx
+++ src/components/SliderSection/SliderSection.tsx
@@ -16,6 +16,9 @@ const SliderSection = () => {
 
   return (
     <div className="my-10">
+      <h2 className="text-2xl text-yellow-500 font-bold ml-16 mb-8 flex justify-between">
+        In Cinemas<span className="text-[10px] md:mr-16 mr-4">Slide left to see more &gt;&gt;&gt;</span>
+      </h2>
       <div className="carousel carousel-end rounded-box">
         {sliderData.map((movie, index) => {
           return <SingleSlide key={index} movie={movie} />;
