5ff95253dc0fce90ebf26e6855c4978d88ab7ee8
sadmanador <sadman.ador@gmail.com>
2024-08-17 00:48:02 +0600

    carousel btn added in the home screen

--- src/components/BannerSection/SingleBanner/SingleBanner.tsx
+++ src/components/BannerSection/SingleBanner/SingleBanner.tsx
@@ -46,31 +46,40 @@ const SingleBanner: React.FC<SingleBannerProps> = ({
           />
         </div>
         <div className="self-end">
-          <h2 className="lg:text-5xl md:text-4x sm:text-3xl text-2x text-white">
+          <h2 className="lg:text-5xl md:text-4x sm:text-3xl text-2xl text-white">
             {movie.title}
           </h2>
           <p className="lg:mt-4 text-stone-400 text-wrap min-w-40 hidden lg:block">
             {movie.overview}
           </p>
           <p className="mt-4 text-stone-400">Vote: {movie.vote_count}</p>
-          <div>
-            <Typography component="legend">
-              Total votes: {movie.vote_count}
-            </Typography>
-            <div
-              className="tooltip tooltip-right"
-              data-tip={(movie.vote_average / 2).toFixed(1) + ` / 5`}
-            >
-              <Rating
-                name="read-only"
-                readOnly
-                value={movie.vote_average / 2}
-                precision={0.5}
-                max={5}
-              />
+          <div className="sm:flex gap-10">
+            <div>
+              <Typography component="legend">
+                Total votes: {movie.vote_count}
+              </Typography>
+              <div
+                className="tooltip tooltip-right"
+                data-tip={(movie.vote_average / 2).toFixed(1) + ` / 5`}
+              >
+                <Rating
+                  name="read-only"
+                  readOnly
+                  value={movie.vote_average / 2}
+                  precision={0.5}
+                  max={5}
+                />
+              </div>
+            </div>
+            <div className="sm:block hidden">
+              <Link
+                href={`/movies/${movie.id}`}
+                className=" bg-[#334155] hover:bg-[#242e3d] text-gray-300 border-none rounded-2xl px-4 py-2 mx-2"
+              >
+                View Details
+              </Link>
             </div>
           </div>
-          <Link href={`/movies/${movie.id}`} className="btn bg-[#334155] hover:bg-[#242e3d]">View Details</Link>
         </div>
       </div>
     </div>
